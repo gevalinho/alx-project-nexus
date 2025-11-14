93d13ea029f4257e308327a8dfc4b55081597abc
Geva-Eval Egbe <gevalinho@gmail.com>
2025-11-14 21:59:36 +0100

    fixing Nativewind bugs

--- alx-project-nexus/app/index.tsx
+++ alx-project-nexus/app/index.tsx
@@ -1,15 +1,20 @@
 import { Text, View } from "react-native";
+import "../global.css";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    // <View
+    //   style={{
+    //     flex: 1,
+    //     justifyContent: "center",
+    //     alignItems: "center",
+    //   }}
+    // >
+    //   <Text>Edit app/index.tsx to edit this screen.</Text>
+    // </View>
+
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-white text-xl font-bold">Profile Screen</Text>
     </View>
   );
 }
